5375e7f15504a2d998b4e496eaf1e0f0e7339900
Adam <adam0tokyo@gmail.com>
2022-08-09 20:38:18 +0900

    fix: rewrite knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,20 +1,38 @@
-require('dotenv').config();
+require('dotenv').config({
+    path: './.env',
+});
 
 module.exports = {
-    client: 'pg',
-    connection: process.env.DATABASE_URL || {
-        host: process.env.DB_HOST || '127.0.0.1',
-        port: process.env.DB_PORT || 5432,
-        database: process.env.DB_NAME,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-    },
+    client: 'postgresql',
+    connection:
+        process.env.DATABASE_URL ||
+        `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@127.0.0.1:5432/fragrapedia`,
     searchPath: 'public',
     migrations: {
-        tableName: 'knex_migrations',
-        directory: __dirname + '/db/migrations',
+        directory: '/db/migrations',
     },
     seeds: {
-        directory: __dirname + '/db/seeds',
+        directory: '/db/seeds',
     },
 };
+
+// require('dotenv').config();
+
+// module.exports = {
+//     client: 'pg',
+//     connection: process.env.DATABASE_URL || {
+//         host: process.env.DB_HOST || '127.0.0.1',
+//         port: process.env.DB_PORT || 5432,
+//         database: process.env.DB_NAME,
+//         user: process.env.DB_USER,
+//         password: process.env.DB_PASSWORD,
+//     },
+//     searchPath: 'public',
+//     migrations: {
+//         tableName: 'knex_migrations',
+//         directory: __dirname + '/db/migrations',
+//     },
+//     seeds: {
+//         directory: __dirname + '/db/seeds',
+//     },
+// };
