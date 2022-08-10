daa56b6a471c69e8f6ea87fd2197bfa5eff69c01
Adam <adam0tokyo@gmail.com>
2022-08-10 12:14:32 +0900

    fix: attempt at normal user_id migration

--- db/migrations/20220723020726_users-create-table.js
+++ db/migrations/20220723020726_users-create-table.js
@@ -3,13 +3,15 @@
  * @returns { Promise<void> }
  */
 exports.up = async function (knex) {
-    await knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+    // await knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
     return await knex.schema.createTable('users', (table) => {
         table
-            .uuid('user_id')
-            .primary()
-            .notNullable()
-            .defaultTo(knex.raw('uuid_generate_v4()'));
+            .increments('user_id')
+            .unique()
+            // .uuid('user_id')
+            // .primary()
+            .notNullable();
+        // .defaultTo(knex.raw('uuid_generate_v4()'));
         table.string('username', 50).notNullable().unique();
         table.string('user_email', 150).notNullable().unique();
         table.string('hashed_password', 150).notNullable();
